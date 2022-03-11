f3c393a152c60fc9c35cec1527e9cf7b49b1f4ba
Moisés Silva <moisesvictor15@gmail.com>
2022-03-11 10:33:11 -0300

    card view update

--- lib/main.dart
+++ lib/main.dart
@@ -12,49 +12,139 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: RandomWords(),
+      theme: ThemeData(          // Add the 5 lines from here...
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home:const RandomWords(),
     );
   }
 }
 
 class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
 
 class _RandomWordsState extends State<RandomWords> {
   @override
-  final _suggestions = <WordPair>[];                 // NEW
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};// NEW
   final _biggerFont = const TextStyle(fontSize: 18); // NEW
+  bool isGrid = true;
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold (                     // Add from here...
+    return Scaffold (
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+          IconButton(
+            icon: const Icon(Icons.grid_view ),
+            onPressed: (){
+              setState(() {
+                isGrid = !isGrid;
+              });
+            },
+            tooltip: 'Change layout',
+          ),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
+  Widget _buildSuggestions() => isGrid
+      ? ListView.builder(
+    padding: const EdgeInsets.all(16),
+    itemBuilder: (context, i) {
+      if (i.isOdd) {
+        return const Divider();
+      }
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    },
+  )
+      :
+  GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount( crossAxisCount: 2, mainAxisSpacing: 10),
+    padding: const EdgeInsets.all(16),
+    itemBuilder: (context, i) {
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return Card(
+        child: _buildRow(_suggestions[i]),
+      );
+
+
+    },
+  );
+
+
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(     // NEW from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {      // NEW lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },               // ... to here.
     );
   }
 }
+
